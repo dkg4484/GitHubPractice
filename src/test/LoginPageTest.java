3db209722674777c012ee2c1c35e59d53ec0b115
dkg4484 <dkg4484@gmail.com>
2021-12-09 19:22:24 +0530

    Login code added

---
+++ src/test/LoginPageTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import pages.LoginPage;
+
+public class LoginPageTest {
+	
+	public static void main(String[] args) {
+		
+		LoginPage login = new LoginPage();
+		
+		login.login();
+		
+	}
+
+}
